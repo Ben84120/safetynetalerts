054528dcfc089b50ed37be4651675e33b71d89a6
Ben <b.vacher@outlook.fr>
2021-07-09 00:43:55 +0200

    Epuration de la classe Jsoniter, implementation des model Person, Firestations et Medical records.

---
+++ src/main/java/net/safety/alert/safety/api/controllers/JsoniterController.java
@@ -0,0 +1,70 @@
+package net.safety.alert.safety.api.controllers;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.hibernate.mapping.Any;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsoniter.JsonIterator;
+
+import net.safety.alert.safety.api.model.FirestastionsModel.FireStation;
+import net.safety.alert.safety.api.model.PersonModel.Person;
+
+//import dataminer.model.FireStation; Pour plus tard
+//import dataminer.model.Person;
+
+@RestController
+public class JsoniterController {
+
+
+    public static void main(String[] args) throws IOException {        
+
+       String filePath = "C:\\Users\\bvach\\git\\safetynetalerts\\resources\\data.json";
+
+    	byte[] bytesFile = Files.readAllBytes(new File(filePath).toPath());
+
+        
+
+    	JsonIterator iter = JsonIterator.parse(bytesFile);
+
+    	com.jsoniter.any.Any any = iter.readAny();
+
+    	com.jsoniter.any.Any personAny = any.get("persons");
+
+    	List<Person> persons = new ArrayList<>();
+
+    	personAny.forEach(a -> persons.add(new Person.PersonBuilder().firstName(a.get("firstName").toString())
+
+    			.address(a.get("address").toString())
+
+    			.city(a.get("city").toString())
+
+    			.lastName(a.get("lastName").toString())
+
+    			.phone(a.get("phone").toString())
+
+    			.zip(a.get("zip").toString())
+
+    			.email(a.get("email").toString())
+
+    			.build()));
+
+    	
+
+    	persons.forEach(p -> System.out.println(p.firstName.concat(p.lastName).concat(p.address).concat(p.city).concat(p.phone).concat(p.zip)));
+    	
+    }    	
+    
+}
+
+    	
+
