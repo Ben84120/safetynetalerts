90201d5a18f38454ff23a7a6957d5f7953fba0bf
Ben <b.vacher@outlook.fr>
2021-07-09 11:04:13 +0200

    Modification du modèle Medicalrecords avec ajout d'un ID

--- src/main/java/net/safety/alert/safety/api/model/MedicalrecordsModel.java
+++ src/main/java/net/safety/alert/safety/api/model/MedicalrecordsModel.java
@@ -1,24 +1,44 @@
 package net.safety.alert.safety.api.model;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
-@Data
+
 public class MedicalrecordsModel {
 	
-	public String firstName;
+	@Data
+	@Entity
+	@Table(name = "medicalrecords")
+	public class Medicalrecords {
+		
+		public class MedicalrecordsBuilder {
+
+		}
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
 	
-	public String lastName;
+	    public String firstName;
 	
-	public Date birthdate;
+	    public String lastName;
 	
-	public String medications;
+	    public Date birthdate;
 	
-	public String allergies;
+	    public String medications;
+	
+	    public String allergies;
 	
 	
 	
 	
 
 	
+}
 }
