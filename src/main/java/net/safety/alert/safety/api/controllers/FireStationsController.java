a2a8a27deffd58a592f88c58664b29d3430fd04f
Ben <b.vacher@outlook.fr>
2021-07-09 11:19:51 +0200

    Refactoring et correction du controller

--- src/main/java/net/safety/alert/safety/api/controllers/FireStationsController.java
+++ src/main/java/net/safety/alert/safety/api/controllers/FireStationsController.java
@@ -1,8 +1,28 @@
 package net.safety.alert.safety.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.safety.alert.safety.api.model.FirestastionsModel.FireStations;
+import net.safety.alerts.safety.api.service.FireStationsService;
 
 @RestController
 public class FireStationsController {
+	
+	@RestController
+	public class FireStationController {
+		@Autowired
+	    private FireStationsService firestationsService;
 
+	    /**
+	    * Read - Get all employees
+	    * @return - An Iterable object of Employee full filled
+	    */
+	    @GetMapping("/firestations")
+	    public Iterable<FireStations> getFirestations() {
+	        return firestationsService.getFireStations();
+	    }
+
+}
 }
