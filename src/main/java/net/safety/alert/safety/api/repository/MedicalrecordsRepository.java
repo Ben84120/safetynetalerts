83c0c5dc4e791318ed1e2a0e42c4c826e1ffb9e8
Ben <b.vacher@outlook.fr>
2021-07-09 11:22:35 +0200

    Refactoring du code

--- src/main/java/net/safety/alert/safety/api/repository/MedicalrecordsRepository.java
+++ src/main/java/net/safety/alert/safety/api/repository/MedicalrecordsRepository.java
@@ -3,10 +3,9 @@ package net.safety.alert.safety.api.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import net.safety.alert.safety.api.model.MedicalrecordsModel;
-
+import net.safety.alert.safety.api.model.MedicalrecordsModel.Medicalrecords;
 
 @Repository
-public interface MedicalrecordsRepository extends CrudRepository<MedicalrecordsModel, Long> {
+public interface MedicalrecordsRepository extends CrudRepository<Medicalrecords, Long> {
 
 }
