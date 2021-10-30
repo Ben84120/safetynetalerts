3d92ab92b30cdf4f116688e22c55402848b66276
Ben <b.vacher@outlook.fr>
2021-10-30 11:00:36 +0200

    Ajout de la dépendance Spring Actuator + ajout des ligndes de codes pour actuator + ajout de la classe LoggingController

---
+++ src/main/java/net/safety/alert/safety/api/model/LoggingController.java
@@ -0,0 +1,22 @@
+package net.safety.alert.safety.api.model;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    org.slf4j.Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Check out the Logs";
+    }
+}
